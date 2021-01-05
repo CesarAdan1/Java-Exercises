d2c95530d6bebdfcec956f3cb850246e8b775872
Cesar Adan <whoclash@hotmail.com>
2021-01-05 13:49:53 -0600

    methods and loops

--- src/com/example/fibonacci/Fibonacci.java
+++ src/com/example/fibonacci/Fibonacci.java
@@ -2,9 +2,17 @@ package com.example.fibonacci;
 
 
 public class Fibonacci {
+    //methods/functions
+     String numberTwo;
+    static void myMethod() {
+        System.out.println("I get married");
+    }
 
     public static void main(String[] args){
+
+
         final int myAge = 28;
+        final String myName = "Cesar";
         final char a = 44, b = 66, c = 67;
         String hello = "Hello World";
         int number = 24;
@@ -15,8 +23,23 @@ public class Fibonacci {
         System.out.println(hello + " " + number + " " + myAge);
         float f1 = 353e3f;
         double f2 = 35E3d;
-        System.out.println(f1);
-        System.out.println(f2);
-        System.out.println(a);
+        String[] cars = {"Volvo", "BMW", "Ford"};
+        for(String i : cars){
+            System.out.println(i);
+        }
+        int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+        for (int i = 0; i < myNumbers.length; ++i) {
+            for(int j = 0; j < myNumbers[i].length; ++j) {
+                System.out.println(myNumbers[i][j]);
+            }
+        }
+        String txt = "ABXASDFDDQPOIWUER123 HEY asdfqwe ASW";
+        String texto = "Mi nombre es \"Cesar\" ";
+
+
+        myMethod();
+
     }
+
 }
+
