76294e8b67a061e91f39a1f8eaba0e9cda7fcb51
Cesar Adan <whoclash@hotmail.com>
2021-01-08 21:51:17 -0600

    matrix ordered ascending and descending

--- src/com/example/orederlistnumbers/OrderListNumbers.java
+++ src/com/example/orederlistnumbers/OrderListNumbers.java
@@ -8,11 +8,25 @@ public class OrderListNumbers {
         int[] numbersArray = {10, 9, 5, 340, 42 , 1, 13, 0};
         int valorMayor = numbersArray[0];
         int valorMenor = numbersArray[0];
-        int listNumbers = numbersArray[0];
+        int num = 0;
 
-       Arrays.sort(numbersArray);
+        for(int n = 0; n < numbersArray.length; n++){
+            for(int m = n + 1; m < numbersArray.length; m++){
+                if(numbersArray[n] > numbersArray[m]){
+                    num = numbersArray[n];
+                    numbersArray[n] = numbersArray[m];
+                    numbersArray[m] = num;
+                }
+            }
+        }
 
-       System.out.println(Arrays.toString(numbersArray));
-
+        System.out.println("La lista ordenada ascendente es: ");
+        for (int n = 0; n < numbersArray.length; n++) {
+            System.out.print(numbersArray[n] + " ");
+        }
+        System.out.println("La lista ordenada descendente es: ");
+        for (int n = numbersArray.length - 1; n > 0; n--) {
+            System.out.print(numbersArray[n] + " ");
+        }
     }
 }
